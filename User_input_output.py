76286c96008aaa9383e239eb4945b5e579c17f9c
Moulya-al <moulyamaruthi6@gmail.com>
2024-12-10 20:14:53 +0530

    Create User_input_output.py

---
+++ User_input_output.py
@@ -0,0 +1,15 @@
+a=5
+b=10
+sum=a+b
+print(sum)
+
+a=int(input('enter first number:'))
+b=int(input('enter second number:'))
+sum=a+b
+print(sum)
+
+
+a=float(input('enter a first number'))
+b+=float(input('enter a second number'))
+sum=a*b
+print(sum)
