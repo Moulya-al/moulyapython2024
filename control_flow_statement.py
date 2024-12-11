f1035689f8b31f6147cbe939c5505328a9dd73e5
Moulya-al <moulyamaruthi6@gmail.com>
2024-12-11 13:27:04 +0530

    Create control_flow_statement.py

---
+++ control_flow_statement.py
@@ -0,0 +1,24 @@
+#if statement
+age=18
+if age>=18:
+    print("you are eligible to vote")
+ 
+  
+#if-else statement
+  num=int(input("enter a number:"))
+if num%2==0:
+    print("number is even")
+else:
+    print("number is odd")
+
+
+  #if-elif-else statement
+  marks=int(input('enter your marks:'))
+if marks>=90:
+   print('grade :A')
+elif marks>75:
+   print('grade:B')
+elif marks>50:
+   print('grade:C')
+else:
+   print('grade:F')
