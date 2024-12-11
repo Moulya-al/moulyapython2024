8e72e96a4b572b95ea2b1719562fc0598895fcf5
Moulya-al <moulyamaruthi6@gmail.com>
2024-12-11 13:34:01 +0530

    Create control_flow_tools.py

---
+++ control_flow_tools.py
@@ -0,0 +1,17 @@
+#break
+for i in range(10):
+   if i==5:
+    break
+   print(i)
+
+#continue
+for i in range(10):
+  if i==5:
+    continue
+  print(i)
+ 
+#pass
+for i in range(10):
+  if i==5:
+    pass
+  print(i)
