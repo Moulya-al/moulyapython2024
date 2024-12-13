3f2a7f666c832b2e7d355b18185c17b462614026
Moulya-al <moulyamaruthi6@gmail.com>
2024-12-13 15:51:39 +0530

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,29 @@
+a={1,2,3,4,5}
+a
+
+#set operations
+#adding items
+a={1,2,3,4,5}
+a.add(6)
+a
+
+#removing items
+a={1,2,3,4,5}
+a.remove(1)
+a
+
+#union
+m={1,2,3,4,5}
+g={6,7,8,9,10}
+m | g
+
+#intersection
+m={1,2,3,4,5,6,7,8}
+g={6,7,8,9,10}
+m&g
+
+#difference
+m={1,2,3,4,5,6,7,8}
+g={2,6,7,8,9,10}
+m-g
+
